5b7d296d96114c11df5ae8a0541cc94b698d4fe2
HEBA ELSAFTY <heba.m.elsafty@gmail.com>
2019-05-03 20:52:05 +0200

    fixed offline

--- sw.js
+++ sw.js
@@ -16,10 +16,17 @@ self.addEventListener("install", function(event) {
       '/js/dbhelper.js',
       '/js/main.js',
       '/js/restaurant_info.js',
-      '/img/*',
+      '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
       '/js/register.js',
-      '//normalize-css.googlecode.com/svn/trunk/normalize.css',
-      'https://fonts.googleapis.com/css?family=Roboto:300,400,500'
     ])
     .catch(error => {
       
